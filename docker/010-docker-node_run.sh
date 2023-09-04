e6b8cb8523c0c6e93ad035c1cea0683b2fae47cd
Malkab <jp.perez.alcantara@gmail.com>
2023-09-04 17:21:15 +0200

    wip

--- docker/010-docker-node_run.sh
+++ docker/010-docker-node_run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mlkdcknoderun \
-  -u 1000:1000 \
-  -i ts_utils_dev \
-  -w $(pwd)/../node \
-  -v $(pwd)/..:$(pwd)/.. \
-  -v $(pwd)/../node/test/assets/:/assets/ \
-  16.13.2
+docker run -ti --rm \
+  --name ts_utils_dev \
+  --hostname ts_utils_dev \
+  --user 1000:1000 \
+  -v $(pwd)/../:$(pwd)/../ \
+  -v ~/.npmrc:/root/.npmrc \
+  -v ~/.npmrc:/home/node/.npmrc \
+  --workdir $(pwd)/../node \
+  registry.gitlab.com/sunnsaas/sunnsaas_v1/sunnsaas-worker-dev:v16
